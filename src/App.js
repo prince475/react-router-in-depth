3080dc2b1db6ad35ba0db23786dc5124c3cee2b8
prince475 <danielmwai475@gmail.com>
2023-05-15 23:29:22 +0300

    Using createBrowserRouter for routing.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import { BrowserRouter, Routes, Route, NavLink }  from "react-router-dom";
+import { createBrowserRouter, Route, NavLink, createRoutesFromElements, RouterProvider }  from "react-router-dom";
 
-//pages
+// pages
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
+import Contact from "./pages/help/Contact";
+import Faq from "./pages/help/Faq";
+import NotFound from "./pages/NotFound";
+import Careers, {careersLoader} from "./pages/careers/Careers";
 
+// Layout
+import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
+import CareersLayout from "./layouts/CareersLayout";
+
+// Loader
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="help" element={<HelpLayout />} >
+          <Route path="faq" element={<Faq/>}/>
+          <Route path="contact" element={<Contact />} />
+        </Route>
+
+        <Route path="careers" element={<CareersLayout />} >
+          <Route
+          index
+          element={<Careers />}
+          loader={ careersLoader }
+          />
+        </Route>
+
+
+        <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+
+)
 
 function App() {
   return (
+      <RouterProvider router={router} />
 
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>JobFix</h1>
-        <NavLink to="/" >Home</NavLink>
-        <NavLink to="About">About</NavLink>
-      </nav>
-    </header>
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
   );
 }
 
