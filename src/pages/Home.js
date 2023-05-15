28a64f35aa62724ddc1348a6255b28a560255f3c
prince475 <danielmwai475@gmail.com>
2023-05-15 07:48:01 +0300

    Creating a home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+        <h2>Welcome</h2>
+        <p>Irure ad enim voluptate do commodo dolore quis fugiat. Dolor proident nisi sint proident ad incididunt est. Nulla veniam minim sunt commodo. Qui culpa minim magna non nulla pariatur dolor mollit sunt reprehenderit.</p>
+
+        <p>Proident ea ea pariatur tempor. Officia eiusmod cillum aliquip ad Lorem deserunt magna aute ut anim excepteur qui consectetur consequat. Non adipisicing qui labore nisi duis do in proident irure minim.</p>
+
+        <p>Sunt qui fugiat exercitation minim et mollit dolore eu. Cillum nostrud ullamco pariatur quis ea Lorem anim in. Id esse reprehenderit consectetur non voluptate aliquip elit duis excepteur ullamco ut tempor. Nostrud consectetur eiusmod non culpa</p>
+    </div>
+  )
+}
