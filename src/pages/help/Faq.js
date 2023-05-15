dcc333f87c371a32bbda66e054c2e32fd9106f46
prince475 <danielmwai475@gmail.com>
2023-05-15 22:59:34 +0300

    Creating Faq component which will be a child of the HelpLayout.

---
+++ src/pages/help/Faq.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Faq() {
+  return (
+    <div className="faq">
+        <h3>Frequently Asked Questions</h3>
+
+        <div className="question">
+            <p><strong>Lorem Ipsum Dolor Sit Amet.</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum dicta aliquam accusamus, est quae, amet, iusto a quam facere dolorum quibusdam. Fuga excepturi voluptate ad quibusdam tempore nulla praesentium!</p>
+        </div>
+
+        <div className="question">
+            <p><strong>Lorem Ipsum Dolor Sit Amet.</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum dicta aliquam accusamus, est quae, amet, iusto a quam facere dolorum quibusdam. Fuga excepturi voluptate ad quibusdam tempore nulla praesentium!</p>
+        </div>
+
+        <div className="question">
+            <p><strong>Lorem Ipsum Dolor Sit Amet.</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum dicta aliquam accusamus, est quae, amet, iusto a quam facere dolorum quibusdam. Fuga excepturi voluptate ad quibusdam tempore nulla praesentium!</p>
+        </div>
+
+
+    </div>
+  )
+}
